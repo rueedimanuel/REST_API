3ddfa35bb52fd9006c0096ab0199f49df5dbcb7b
Manuel <m4nuel.r@gmail.com>
2025-01-18 19:26:18 +0100

    Release 2.0

--- Desktop_Qt_6_8_1_MinGW_64_bit-Release/.qt/deploy_REST_API_9122335b0c.cmake
+++
@@ -1,8 +0,0 @@
-include(C:/Users/M4nue/Documents/QtDesignStudio/Projects/REST_API/build/Desktop_Qt_6_8_1_MinGW_64_bit-Release/.qt/QtDeploySupport.cmake)
-include("${CMAKE_CURRENT_LIST_DIR}/REST_API-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE C:/Users/M4nue/Documents/QtDesignStudio/Projects/REST_API/build/Desktop_Qt_6_8_1_MinGW_64_bit-Release/REST_API.exe
-    GENERATE_QT_CONF
-)
